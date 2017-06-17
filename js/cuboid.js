93b990ce50c8e240c3c0e95aff68c9b44cefae3e
wishedy <zhfirefly@yahoo.com>
2017-06-17 20:41:18 +0800

    自制长方形游戏初稿

--- js/cuboid.js
+++ js/cuboid.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
         origin:$(".cuboid-drag-station"),
         dragItem:$(".cuboid-drag-item"),
         target:$(".cuboid-drop-item"),
+        successBar:$(".cuboid-drop-board"),
         dragActive:null,
         dragBar:$(".cuboid-drag-bar"),
         audio:document.getElementById('audio')
@@ -15,11 +16,11 @@ $(document).ready(function(){
         leftDis: [],
         widthGap: 0,
         heightGap:0,
-        allowError: 2,
+        allowError: 20,
         topDis: [],
         originalLeft:[],
         originalTop:[],
-        passNum:4
+        passNum:6
     };
     var cuboid = {
         init:function(){
@@ -44,12 +45,16 @@ $(document).ready(function(){
         storePosition:function(){
           var t = this;
             element.dragItem.each(function(){
-                parameter.leftDis.push(parseInt($(this).offset().left));
+                parameter.originalLeft.push($(this).offset().left);
                 parameter.originalTop.push(parseInt($(this).offset().top));
             });
-            parameter.originalLeft = parameter.leftDis;
             element.target.each(function(){
-                parameter.topDis.push(parseInt($(this).offset().top))
+                var jsonItem = {};
+                jsonItem.topPx = parseInt($(this).offset().top);
+                parameter.leftDis.push(parseInt($(this).offset().left));
+                jsonItem.leftPx = parseInt($(this).offset().left);
+                jsonItem.child = null;
+                parameter.topDis.push(jsonItem)
             });
             return t;
         },
@@ -70,15 +75,23 @@ $(document).ready(function(){
                     return false;
                 }
             });
-            $.each(parameter.topDis,function(i,v){
-                var actualGap = ((v-resultTop)>0)?(v-resultTop):(v-resultTop)*(-1);
+            function judgeTopOnOff(){
+                var item = parameter.topDis[element.dragActive.index()];
+                var topPx = item.topPx;
+                var actualGap = ((topPx-resultTop)>0)?(topPx-resultTop):(topPx-resultTop)*(-1);
                 var errorHeight = actualGap-parameter.heightGap;
                 topOnOff = errorHeight<parameter.allowError;
                 if(topOnOff){
-                    locationTop = v;
-                    return false;
+                    if(item.child!=null){
+                        topOnOff = false;
+                        return false;
+                    }else{
+                        locationTop = topPx;
+                        return false;
+                    }
                 }
-            });
+            }
+            judgeTopOnOff();
             var resultJson = {"dropOnOff":leftOnOff&&topOnOff,posLeft:locationLeft,posTop:locationTop}
             return resultJson;
         },
@@ -87,6 +100,16 @@ $(document).ready(function(){
             var restoreTop = parameter.originalTop[element.dragActive.index()];
             var restoreLeft = parameter.originalLeft[element.dragActive.index()];
             element.dragActive.offset({top:restoreTop,left:restoreLeft}) ;
+            element.audio.src="audio/lib_965_5531.mp3";
+            element.audio.play();
+            return t;
+        },
+        successFn:function(){
+            var t = this;
+            element.dragItem.fadeOut(1000);
+            element.successBar.addClass("cuboid-drop-success");
+            element.target.addClass("cuboid-drop-unify");
+            element.audio.src="audio/lib_6287_6601.mp3";
             element.audio.play();
             return t;
         },
@@ -99,23 +122,20 @@ $(document).ready(function(){
                     start: function() {
                         $(this).attr({"data-drop":"false"});
                         element.dragActive = $(this);
+                        parameter.originalLeft[element.dragActive.index()] = $(this).offset().left;
+                        parameter.originalTop[element.dragActive.index()] = $(this).offset().top;
                     },
                     stop:function(){
                         var dropInfo = t.dropInOnOff();
                         if(!dropInfo.dropOnOff){
                             t.restorePos();
                         }
+                        if(JSON.parse(element.dragActive.attr("data-drop"))){
+                            parameter.topDis[element.dragActive.index()].child=element.dragActive;
+                        }
                         var lastResult = $("[data-drop='true']").length;
                         if(lastResult==parameter.passNum){
-                            element.dragItem.fadeOut(1000);
-                            element.target.addClass("cuboid-drop-unify");
-                            //$("#musicbox").attr({"src":decodeURI(arrMusic[m])});
-                            element.audio.src="audio/lib_6287_6601.mp3";
-                            element.audio.play();
-                            //musicbox.play();
-                            /*$(".cuboid-mask-layer").append('<audio src="/audio/lib_6287_6601.mp3"></audio>');
-                            element.audio.remove();
-                            $("audio")[0].play();*/
+                            t.successFn();
                         }
                     }
                 });
@@ -126,10 +146,13 @@ $(document).ready(function(){
                     var locationLeft = dropInfo.posLeft;
                     var locationTop = dropInfo.posTop;
                     if(dropOnff){
-                        element.dragActive.attr({"data-drop":"true"});
-                        element.dragActive.offset({top:locationTop,left:locationLeft}) ;
-                        element.audio.src="audio/lib_6287_6601.mp3";
-                        element.audio.play();
+                        if(!JSON.parse(element.dragActive.attr("data-drop"))){
+                            element.dragActive.attr({"data-drop":"true"});
+                            element.dragActive.offset({top:locationTop,left:locationLeft}) ;
+                            element.audio.src="audio/lib_6287_6601.mp3";
+                            element.audio.play();
+                        }
+
                     }else{
                         t.restorePos();
                     }
